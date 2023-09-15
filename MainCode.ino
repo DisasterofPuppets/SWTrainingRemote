83af8c1e10e2df6d90f56a612996d38e28c6039a
Disaster of Puppets <62915210+DisasterofPuppets@users.noreply.github.com>
2023-09-15 10:27:21 +1000

    Update MainCode.ino

--- MainCode.ino
+++ MainCode.ino
@@ -5,134 +5,22 @@
 
 CRGB leds[NUM_LEDS];
 
-
-
-int sound_1 = 4;
-int sound_2 = 5;
-int sound_3 = 6;
-int sound_4 = 7;
-int sound_5 = 8;
-int sound_6 = 9;
-
-
-String song;
-int randomdelay;
+int soundPins[] = {4, 5, 6, 7, 8, 9};
+int song = 0; // Store the selected song
 
 void setup() {
-  // put your setup code here, to run once:
-
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(100); // Adjust this value for desired overall brightness
   randomSeed(analogRead(0)); // Seed the random number generator
 
-Serial.begin(9600);
-pinMode(sound_1, OUTPUT);
-pinMode(sound_2, OUTPUT);
-pinMode(sound_3, OUTPUT);
-pinMode(sound_4, OUTPUT);
-pinMode(sound_5, OUTPUT);
-pinMode(sound_6, OUTPUT);
-
-digitalWrite(DATA_PIN, LOW);
-digitalWrite(sound_1,HIGH);
-digitalWrite(sound_2,HIGH);
-digitalWrite(sound_3,HIGH);
-digitalWrite(sound_4,HIGH);
-digitalWrite(sound_5,HIGH);
-digitalWrite(sound_6,HIGH);
-
-
-Serial.print("All off");
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-    song = Serial.available();
-    song.trim();
-    Serial.print("Song is = ");
-    Serial.println(song);
-    randomdelay = random(5000,15000);
-    delay(randomdelay);
- 
-
-Dimmer();
-  
-  song = random(1,6);
- 
- 
-  if(song=="1")
-  {
-    digitalWrite(sound_1,LOW);
-    digitalWrite(sound_2,HIGH);
-    digitalWrite(sound_3,HIGH);
-    digitalWrite(sound_4,HIGH);
-    digitalWrite(sound_5,HIGH);
-    digitalWrite(sound_6,HIGH);
-    Serial.println("Track 1");
-    delay(5000);
+  // Initialize sound pins
+  for (int i = 0; i < NUM_LEDS; i++) {
+    pinMode(soundPins[i], OUTPUT);
+    digitalWrite(soundPins[i], HIGH); // Turn off all sound modules initially
   }
 
-  else if(song=="2")
-  {
-    digitalWrite(sound_1,HIGH);
-    digitalWrite(sound_2,LOW);
-    digitalWrite(sound_3,HIGH);
-    digitalWrite(sound_4,HIGH);
-    digitalWrite(sound_5,HIGH);
-    digitalWrite(sound_6,HIGH);
-    Serial.println("Track 2");
-    delay(2000);
-  }
-
-    else if(song=="3")
-  {
-    digitalWrite(sound_1,HIGH);
-    digitalWrite(sound_2,HIGH);
-    digitalWrite(sound_3,LOW);
-    digitalWrite(sound_4,HIGH);
-    digitalWrite(sound_5,HIGH);
-    digitalWrite(sound_6,HIGH);
-    Serial.println("Track 3");
-    delay(6000);
-  }
-
-    else if(song=="4")
-  {
-    digitalWrite(sound_1,HIGH);
-    digitalWrite(sound_2,HIGH);
-    digitalWrite(sound_3,HIGH);
-    digitalWrite(sound_4,LOW);
-    digitalWrite(sound_5,HIGH);
-    digitalWrite(sound_6,HIGH);
-    Serial.println("Track 4");
-    delay(2000);
-    }
-
-    else if(song=="5")
-  {
-    digitalWrite(sound_1,HIGH);
-    digitalWrite(sound_2,HIGH);
-    digitalWrite(sound_3,HIGH);
-    digitalWrite(sound_4,HIGH);
-    digitalWrite(sound_5,LOW);
-    digitalWrite(sound_6,HIGH);
-    Serial.println("Track 5");
-    delay(3000);
-  }
-
-    else if(song=="6")
-  {
-    digitalWrite(sound_1,HIGH);
-    digitalWrite(sound_2,HIGH);
-    digitalWrite(sound_3,HIGH);
-    digitalWrite(sound_4,HIGH);
-    digitalWrite(sound_5,HIGH);
-    digitalWrite(sound_6,LOW);
-    Serial.println("Track 6");
-    delay(3000);
-  }
-
+  Serial.begin(9600);
+  Serial.println("All off");
 }
 
 // Function to shuffle an array randomly
@@ -148,14 +36,42 @@ void shuffleArray(int arr[], int size) {
   }
 }
 
-void Dimmer() // 
-{
-// Set LEDs to random color and brightness
+void loop() {
+  // Select a random song (1 to 6)
+  song = random(1, 7);
+  Serial.print("Selected song: ");
+  Serial.println(song);
+
+  // Play the selected song
+  playSong(song);
+
+  // Run the LED pattern
+  Dimmer();
+}
+
+void playSong(int songNumber) {
+  // Turn off all sound modules initially
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(random(256), random(256), random(256));
-    FastLED.show();
-    delay(50); // Delay to visually appreciate the random colors
+    digitalWrite(soundPins[i], HIGH);
   }
+
+  // Turn on the sound module corresponding to the selected song
+  if (songNumber >= 1 && songNumber <= NUM_LEDS) {
+    digitalWrite(soundPins[songNumber - 1], LOW);
+    Serial.print("Playing song ");
+    Serial.println(songNumber);
+  } else {
+    Serial.println("Invalid song number");
+  }
+
+  // Add any additional logic for sound playback here
+}
+
+void Dimmer() {
+  // Set one LED to be always on
+  int alwaysOnLED = random(NUM_LEDS);
+  bool alwaysOnBlink = false; // Flag to control blinking of always-on LED
+  int alwaysOnColorChangeDelay = random(2000, 5000); // Random delay for color change
 
   // Randomly determine whether to blink slowly or quickly
   bool slowBlink = random(2) == 0;
@@ -163,19 +79,22 @@ void Dimmer() //
   // Randomly choose the number of LEDs to flicker quickly
   int numLEDsToFlickerQuickly = random(1, 2); // 1 or 2
 
-  // Ensure at least 3 LEDs are turned on and they are unique
-  int numLEDsToKeepOn = max(3, random(NUM_LEDS - numLEDsToFlickerQuickly + 1));
-  int indices[NUM_LEDS]; // Array to store LED indices
+  // Ensure at least 2 more LEDs are turned on and they are unique
+  int numLEDsToKeepOn = max(2, random(NUM_LEDS - numLEDsToFlickerQuickly));
+  int indices[NUM_LEDS - 1]; // Array to store LED indices except the always-on LED
+
+  // Fill the array with indices 0 to NUM_LEDS-1, excluding alwaysOnLED
+  int j = 0;
   for (int i = 0; i < NUM_LEDS; i++) {
-    indices[i] = i; // Fill the array with indices 0 to NUM_LEDS-1
+    if (i != alwaysOnLED) {
+      indices[j] = i;
+      j++;
+    }
   }
-  shuffleArray(indices, NUM_LEDS); // Shuffle the indices randomly
 
-  // Turn off all LEDs initially
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
+  shuffleArray(indices, NUM_LEDS - 1); // Shuffle the indices randomly
 
-  // Turn on at least 3 unique LEDs with random colors
+  // Turn on at least 2 unique LEDs with random colors
   for (int i = 0; i < numLEDsToKeepOn; i++) {
     int index = indices[i];
     leds[index] = CRGB(random(256), random(256), random(256)); // Random color
@@ -183,38 +102,33 @@ void Dimmer() //
 
   // Flicker selected LEDs with changing colors
   for (int i = 0; i < numLEDsToFlickerQuickly; i++) {
-    int index = random(NUM_LEDS);
+    int index = random(NUM_LEDS - 1); // Random index except the always on LED
     for (int j = 0; j < 10; j++) { // Change colors quickly for 10 iterations
-      leds[index] = CRGB(random(256), random(256), random(256)); // Change color
+      leds[indices[index]] = CRGB(random(256), random(256), random(256)); // Change color
       FastLED.show();
       delay(100); // Delay to control the color change speed
     }
-    leds[index] = CRGB::Black; // Turn off the LED
+    leds[indices[index]] = CRGB::Black; // Turn off the LED
   }
 
-  // Randomly flash some LEDs and dim others
-  for (int i = 0; i < NUM_LEDS; i++) {
-    if (random(2) == 0) { // 50% chance to flash
-      leds[i] = CRGB::White; // Flash with full brightness (white)
-      FastLED.show();
-      if (slowBlink) {
-        delay(random(800, 2000)); // Random delay for slow blinking
-      } else {
-        delay(random(50, 500)); // Random delay for quick blinking
-      }
-      leds[i] = CRGB::Black; // Turn off the LED
-      FastLED.show();
+  // Blink or fade the always-on LED
+  if (alwaysOnBlink) {
+    if (slowBlink) {
+      delay(random(800, 2000)); // Random delay for slow blinking
     } else {
-      // Dim the LED to 0 at a lower speed
-      for (int b = 255; b >= 0; b--) {
-        leds[i].fadeToBlackBy(random(1, 4)); // Random dim speed
-        FastLED.show();
-        delay(20); // Delay to control the slower dimming speed
-      }
+      delay(random(50, 500)); // Random delay for quick blinking
     }
+    alwaysOnBlink = !alwaysOnBlink;
+  } else {
+    // Dim the always-on LED to 0 at a lower speed
+    for (int b = 255; b >= 0; b--) {
+      leds[alwaysOnLED].fadeToBlackBy(random(1, 4)); // Random dim speed
+      FastLED.show();
+      delay(20); // Delay to control the slower dimming speed
+    }
+    delay(alwaysOnColorChangeDelay); // Delay before changing the color of always-on LED
+    leds[alwaysOnLED] = CRGB(random(256), random(256), random(256)); // Random color
+    FastLED.show();
+    alwaysOnColorChangeDelay = random(2000, 5000); // Random delay for color change
   }
-
 }
-
-
-
